0f75e68fc4cc5fc687af49b5bcfdf6f7a1c9f389
varandaas <varandaas@POSITIVEVIBES.Linux>
2016-03-04 15:25:17 +0000

    get machine ip

--- schat.c
+++ schat.c
@@ -19,7 +19,7 @@
  *  returns:  -1 always (when this runs, it means something about the user's entered arguments went wrong)
  */
 int show_usage(){
-	printf("Usage: schat –n name.surname –i ip -p scport -s snpip -q snpport\n");
+	printf("Usage: schat –n name.surname [–i ip] [-p scport] -s snpip -q snpport\n");
 	return -1;
 
 }
@@ -44,6 +44,10 @@ int show_usage(){
  */
 
 int main(int argc, char* argv[]) {
+	FILE * fp_ip; // file for getting own ip
+	char ip_path[1035], garb_0[1035], ip[1035];
+	int lineCount = 2;
+
 	struct hostent *h;
 	char options[10] = "n:s:q:i:p:";
 	char *in_name_surname = NULL ,*in_ip = NULL ,*in_scport = NULL,*in_snpip = NULL,*in_snpport = NULL;
@@ -90,14 +94,41 @@ int main(int argc, char* argv[]) {
 		}
 	}
 	/* Check if required arguments are given   */
-	if(in_name_surname == NULL) {
+	if(in_name_surname == NULL || in_snpip == NULL || in_snpport == NULL) {
 		show_usage();
 		return -1;
 	}
 
-	printf("YO\n");
+	if(in_ip == NULL) {
+		/* Open the command for reading. */
+		fp_ip = popen("ip a | grep wlan", "r");
+		if (fp_ip == NULL) {
+			printf("Cannot get local ip address.\n" );
+			return(-1);
+		}
+
+		/* Read the output a line at a time - output it. */
+		while (fgets(ip_path, sizeof(ip_path)-1, fp_ip) != NULL) {
+			lineCount--;
+			if (lineCount == 0){
+				sscanf(ip_path,"%s %[^'/']%s",garb_0,ip);
+			}
+		}
+
+		pclose(fp_ip);
+
+		printf("%s\n", ip);
+
+		in_ip = ip;
+	}
+
+	if(in_scport == NULL) {
+		in_scport = "6000";
+	}
 
 	
+
+	printf("YO\n");
 
 	return 0;	
 }
