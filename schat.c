b2ca2278c3785b69e0f210ce4b0cbd4a20bdcd90
varandaas <varandaas@POSITIVEVIBES.Linux>
2016-03-26 08:03:57 +0000

    fully functioning chat

--- schat.c
+++ schat.c
@@ -22,7 +22,8 @@ typedef int bool;
 // Program States
 #define init 0 // Initial State
 #define registered 1
-#define onChat 2 
+#define onChat_received 2
+#define onChat_sent 3 
 
 int stateMachine = init;
 
@@ -220,12 +221,12 @@ int main(int argc, char* argv[]) {
 	char * name2connect;
 	char * location;
 
-	int fd, addrlen, newfd;
+	int fd = NULL, addrlen, newfd = NULL;
 	struct sockaddr_in addr;
 	int n, nw;
 	char *ptr;
 
-	int fd_out;
+	int fd_out = NULL;
 	struct sockaddr_in addr_out;
 	char * dst_ip = "127.0.0.1";
 	struct in_addr temp;
@@ -306,20 +307,184 @@ int main(int argc, char* argv[]) {
 		FD_ZERO(&rfds);
 		FD_SET(fileno(stdin),&rfds);
 		FD_SET(fd,&rfds);
-		
+		FD_SET(newfd,&rfds);
+		FD_SET(fd_out,&rfds);
+			
 		maxfd = fileno(stdin);
 		maxfd = max(maxfd,fd);
+		maxfd = max(maxfd,newfd);
+		maxfd = max(maxfd,fd_out);
+
+ 		memset(usrIn, 0, sizeof(usrIn));
 
 		counter=select(maxfd+1,&rfds,(fd_set*)NULL,(fd_set*)NULL,(struct timeval *)NULL);
  		
+		printf("LHE DESBLOQUEIA\n");
+
  		if(counter == -1){
  			printf("ERROR-: %s\n",strerror(errno));
  			exit(1);
  		}
 
+
  		if(FD_ISSET(fileno(stdin),&rfds)){
 			printf("YOO\n");
 			fgets(usrIn,100,stdin);
+
+			if (strcmp(usrIn,"join\n") == 0 && stateMachine == init){
+				usrRegister(in_snpip, in_snpport,in_name_surname,in_ip,in_scport);
+				/* close(fd); exit(0); */
+
+			}else if(strcmp(usrIn,"leave\n") == 0){
+				usrExit(in_snpip, in_snpport,in_name_surname);
+				close(newfd);
+
+			}else if(strstr(usrIn,"find") != NULL){
+
+				memset(buffer, 0, sizeof(buffer));
+
+				sscanf(usrIn,"find %s", buffer);
+
+				name2connect = malloc(strlen(buffer));
+
+				name2connect = buffer;
+
+				if (strlen(name2connect) <= 1){
+					printf("Argument missing. Who do you want to talk to?\n");
+				}else{
+					location = queryUser(in_snpip, in_snpport, name2connect);
+
+					if (location == NULL){
+						printf("Ups.. User not found.\n");
+					}else{
+						printf("User located at: %s\n", location);
+					}
+				}
+
+			}else if(strstr(usrIn,"connect") != NULL){
+
+				printf("Trying to locate user..\n");
+
+				memset(buffer, 0, sizeof(buffer));
+
+				sscanf(usrIn,"connect %s", buffer);
+
+				name2connect = malloc(strlen(buffer));
+				name2connect = buffer;
+
+				if (strlen(name2connect) <= 1){
+					printf("Argument missing. Who do you want to talk to?\n");
+				}else{
+					location = queryUser(in_snpip, in_snpport, name2connect);
+
+					if (location == NULL){
+						printf("Ups.. User not found.\n");
+					}else{
+						printf("User located at: %s\n", location);
+						
+						printf("Trying to connect..\n");
+					}	
+				}
+
+				strcpy(buffer,location);
+
+				sscanf(buffer,"%[^';'];%s",remote_ip, remote_port);
+
+				fd_out=socket(AF_INET,SOCK_STREAM,0);//TCP socket
+
+				if(fd_out==-1){
+					printf("FAIL\n");
+					return -1;//error
+				}
+
+				inet_pton(AF_INET, remote_ip, &temp);
+				h=gethostbyaddr(&temp,sizeof(temp),AF_INET);
+				a=(struct in_addr*)h->h_addr_list[0];
+
+				memset((void*)&addr_out,(int)'\0',sizeof(addr_out));
+
+				addr_out.sin_family=AF_INET;
+				addr_out.sin_addr=*a;
+				addr_out.sin_port=htons(atoi(remote_port));
+
+				n=connect(fd_out,(struct sockaddr*)&addr_out,sizeof(addr_out));
+
+				if (n == -1){
+					printf("ERROR connecting\n");
+					return -1;
+				}
+
+				stateMachine = onChat_sent;
+	/*
+				if((fd=socket(AF_INET,SOCK_STREAM,0))==-1)exit(1);//error
+				
+					memset((void*)&addr,(int)'\0',sizeof(addr));
+					addr.sin_family=AF_INET;
+					addr.sin_addr.s_addr=htonl(INADDR_ANY);
+					addr.sin_port=htons(atoi(in_snpport));
+					
+					if(bind(fd,(struct sockaddr*)&addr,sizeof(addr))==-1)
+						exit(1);//error
+					
+					if(listen(fd,5)==-1)exit(1);//error
+					
+					while(1){addrlen=sizeof(addr);
+					
+					if((newfd=accept(fd,(struct sockaddr*)&addr,&addrlen))==-1)
+						exit(1);//error
+					
+					while((n=read(newfd,buffer,128))!=0){if(n==-1)exit(1);//error
+						ptr=&buffer[0];
+					
+					while(n>0){if((nw=write(newfd,ptr,n))<=0)exit(1);//error
+						n-=nw; ptr+=nw;}
+					}
+						close(newfd);
+					}*/
+			}else if(strstr(usrIn,"message") != NULL){
+
+				sscanf(usrIn,"message %[^\n]", buffer);
+
+				printf("Sending message: %s\n",buffer);
+
+				if (stateMachine == onChat_sent){
+
+					nw = write(fd_out,buffer,strlen(buffer));
+
+				}else if(stateMachine == onChat_received){
+					
+					nw = write(newfd,buffer,strlen(buffer));
+
+				}
+
+				if (nw <= 1){
+					printf("Could not send message.\n");
+				}
+
+				/*
+				if (stateMachine == onChat_sent){
+					
+				}else if(stateMachine == onChat_received){
+					write(newfd,buffer,strlen(buffer));
+				}*/
+
+				memset(buffer, 0, sizeof(buffer));
+
+			}else if(strcmp(usrIn,"disconnect\n") == 0){
+
+			}else if(strcmp(usrIn,"exit\n") == 0){
+
+				// checkar se nao fizemos leave antes !!!!!!!!!!!!! - maquina de estados
+				usrExit(in_snpip, in_snpport,in_name_surname);
+				close(newfd);
+				close(fd);
+				close(fd_out);
+
+				printf("Exiting..\n");
+				break;
+			}else{
+				printf("You are not allowed to do that my friend.\n");
+			}
 		}
 
 		if(FD_ISSET(fd,&rfds)){
@@ -331,25 +496,23 @@ int main(int argc, char* argv[]) {
 				printf("ERROR: Cannot start listening TCP.\n");
 				exit(1); //error
 			}
-
-			FD_SET(newfd,&rfds);
-			maxfd = max(maxfd,newfd);
-			
-
 		}
 
 		if (newfd != NULL){
+
+			stateMachine = onChat_received;
 
 			if(FD_ISSET(newfd,&rfds)){
 				printf("YOO ACCEPT YOO\n");
 					
 				memset(buffer, 0, sizeof(buffer));
 
-				while((n=read(newfd,buffer,512))!=0){
+				n=read(newfd,buffer,512);
 
-				 	if(n==-1)
-				 		break;//error
-					printf("BAM: %s\n", buffer);
+				if(n==-1)
+				 	break;//error
+
+				printf("BAM: %s\n", buffer);
 
 				 	/*ptr=&buffer[0];
 
@@ -363,152 +526,27 @@ int main(int argc, char* argv[]) {
 					 	n-=nw; ptr+=nw;
 					 	printf("x");
 					}*/
-				}
 			}
 		}
 
 		if (fd_out != NULL){
 			if(FD_ISSET(fd_out,&rfds)){
+				printf("Don't worry mate\n");
 
+				memset(buffer, 0, sizeof(buffer));
+
+				n=read(fd_out,buffer,512);
+
+				if(n==-1)
+				 	break;//error
+
+				printf("AI NAO: %s\n", buffer);
 			}
 		}
 
 		printf("BAZOU\n");
 
-		if (strcmp(usrIn,"join\n") == 0 && stateMachine == init){
-			usrRegister(in_snpip, in_snpport,in_name_surname,in_ip,in_scport);
-			/* close(fd); exit(0); */
-
-		}else if(strcmp(usrIn,"leave\n") == 0){
-			usrExit(in_snpip, in_snpport,in_name_surname);
-			close(newfd);
-
-		}else if(strstr(usrIn,"find") != NULL){
-
-			memset(buffer, 0, sizeof(buffer));
-
-			sscanf(usrIn,"find %s", buffer);
-
-			name2connect = malloc(strlen(buffer));
-
-			name2connect = buffer;
-
-			if (strlen(name2connect) <= 1){
-				printf("Argument missing. Who do you want to talk to?\n");
-			}else{
-				location = queryUser(in_snpip, in_snpport, name2connect);
-
-				if (location == NULL){
-					printf("Ups.. User not found.\n");
-				}else{
-					printf("User located at: %s\n", location);
-				}
-			}
-
-		}else if(strstr(usrIn,"connect") != NULL){
-
-			printf("Trying to locate user..\n");
-
-			memset(buffer, 0, sizeof(buffer));
-
-			sscanf(usrIn,"connect %s", buffer);
-
-			name2connect = malloc(strlen(buffer));
-			name2connect = buffer;
-
-			if (strlen(name2connect) <= 1){
-				printf("Argument missing. Who do you want to talk to?\n");
-			}else{
-				location = queryUser(in_snpip, in_snpport, name2connect);
-
-				if (location == NULL){
-					printf("Ups.. User not found.\n");
-				}else{
-					printf("User located at: %s\n", location);
-					
-					printf("Trying to connect..\n");
-				}	
-			}
-
-			strcpy(buffer,location);
-
-			sscanf(buffer,"%[^';'];%s",remote_ip, remote_port);
-
-			fd_out=socket(AF_INET,SOCK_STREAM,0);//TCP socket
-
-			if(fd_out==-1){
-				printf("FAIL\n");
-				return -1;//error
-			}
-
-			inet_pton(AF_INET, remote_ip, &temp);
-			h=gethostbyaddr(&temp,sizeof(temp),AF_INET);
-			a=(struct in_addr*)h->h_addr_list[0];
-
-			memset((void*)&addr_out,(int)'\0',sizeof(addr_out));
-
-			addr_out.sin_family=AF_INET;
-			addr_out.sin_addr=*a;
-			addr_out.sin_port=htons(atoi(remote_port));
-
-			n=connect(fd_out,(struct sockaddr*)&addr_out,sizeof(addr_out));
-
-			if (n == -1){
-				printf("ERROR connecting\n");
-				return -1;
-			}
-
-			if(stateMachine == onChat){
-/*
-			if((fd=socket(AF_INET,SOCK_STREAM,0))==-1)exit(1);//error
-			
-				memset((void*)&addr,(int)'\0',sizeof(addr));
-				addr.sin_family=AF_INET;
-				addr.sin_addr.s_addr=htonl(INADDR_ANY);
-				addr.sin_port=htons(atoi(in_snpport));
-				
-				if(bind(fd,(struct sockaddr*)&addr,sizeof(addr))==-1)
-					exit(1);//error
-				
-				if(listen(fd,5)==-1)exit(1);//error
-				
-				while(1){addrlen=sizeof(addr);
-				
-				if((newfd=accept(fd,(struct sockaddr*)&addr,&addrlen))==-1)
-					exit(1);//error
-				
-				while((n=read(newfd,buffer,128))!=0){if(n==-1)exit(1);//error
-					ptr=&buffer[0];
-				
-				while(n>0){if((nw=write(newfd,ptr,n))<=0)exit(1);//error
-					n-=nw; ptr+=nw;}
-				}
-					close(newfd);
-				}*/
-			}
-		}else if(strstr(usrIn,"message") != NULL){
-
-			sscanf(usrIn,"message %s\n", buffer);
-
-			write(fd_out,buffer,strlen(buffer)); //stdout
-
-			memset(buffer, 0, sizeof(buffer));
-
-		}else if(strcmp(usrIn,"disconnect\n") == 0){
-
-		}else if(strcmp(usrIn,"exit\n") == 0){
-
-			// checkar se nao fizemos leave antes !!!!!!!!!!!!! - maquina de estados
-			usrExit(in_snpip, in_snpport,in_name_surname);
-			close(newfd);
-			close(fd);
-			close(fd_out);
-
-			printf("Exiting..\n");
-			break;
-		}else{
-			printf("You are not allowed to do that my friend.\n");
-		}
+		
 	}
 
 	printf("\n\nThe End\n\n\n");
