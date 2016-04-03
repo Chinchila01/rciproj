6390bedf1ebbc13ab0a0555a1f07691410d414e8
Filipe Correia <filipe.martires.correia@tecnico.ulisboa.pt>
2016-04-03 22:06:22 +0100

    Bug fixes, removing unnecessary libraries from snp

--- snp.c
+++ snp.c
@@ -1,15 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-//#include <string.h>
-//#include <unistd.h>
 #include <ctype.h>
-//#include <netdb.h>
-//#include <sys/types.h>
-//#include <sys/socket.h>
-//#include <netinet/in.h>
-//#include <arpa/inet.h>
-//#include <string.h>
-//#include <errno.h>
 #include "helper.h"
 #include "communications.h"
 
