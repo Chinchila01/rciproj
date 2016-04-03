47d92599c4c39375812a82919fa6e412926a1d56
Francisco Varandas de Sousa <varandaas@POSITIVEVIBES.Linux>
2016-04-03 17:41:12 +0100

    added comments and some error checks

--- schat.c
+++ schat.c
@@ -52,25 +52,28 @@ int ncmd = sizeof(commands)/sizeof(commands[0]); /* Number of commands */
 /* Name of file where default hashtable is located */
 #define HASHFILE "hashtable.txt"
 
-#define max(A,B) ((A)>=(B)?(A):(B))
+#define max(A,B) ((A)>=(B)?(A):(B)) // returns max value between A and B
 
+// define boolean variable types
 typedef int bool;
 #define true 1
 #define false 0
 
 /* Program States */
-#define init 0 /* Initial State */
-#define registered 1
-#define onChat_received 2
-#define onChat_sent 3
-#define onChat_authenticating_step_1 4 
+
+#define init 0 // initial state, not registered on network. No network actions allowed
+#define registered 1 // after join, can leave, find users and connect
+#define onChat_received 2 // state for incoming connection
+#define onChat_sent 3 // state for outgoing connection
+// authentication steps for sequential message exchange
+#define onChat_authenticating_step_1 4
 #define onChat_authenticating_step_2 5
 #define onChat_authenticating_step_3 6
 
-/* Name of file where hashtable is located */
+/* Name of file where hashtable is located, can be updated during execution */
 char * HASHTABLE = "hashtable.txt";
 
-int stateMachine = init;
+int stateMachine = init; // define initial state
 
 struct stat st = {0}; /* Sincerely, why do I need this? */
 
@@ -165,21 +168,26 @@ bool encrypt(unsigned char* encrypted, unsigned char c) {
 	char buffer[512];
 	int i;
 
-	table = fopen(HASHTABLE,"r");
+	table = fopen(HASHTABLE,"r"); // open key file
 
+	// show error message if cannot open
 	if(table == NULL) {
 		printf(ANSI_COLOR_RED "encrypt: error: %s",strerror(errno));
 		printf(ANSI_COLOR_WHITE "\n");
 		encrypted = NULL;
+
+		// set key file to default
 		HASHTABLE = malloc(strlen(HASHFILE));
 		strcpy(HASHTABLE,HASHFILE);
 		return false;
 	}
 
+	// read file until c line
 	for(i = 0;i < c+1 ; i++){
 		fgets(buffer,512,table);
 	}
 
+	// get encrypted char, store it on memory and deliver error if file bad formated
 	if(sscanf(buffer,"%u",encrypted) != 1) {
 		printf(ANSI_COLOR_RED "encrypt: error parsing file");
 		printf(ANSI_COLOR_WHITE "\n");
@@ -187,7 +195,7 @@ bool encrypt(unsigned char* encrypted, unsigned char c) {
 		return false;
 	}
 		
-	fclose(table);
+	fclose(table); // close file
 
 	return true;
 }
@@ -220,6 +228,7 @@ char * comUDP(char * msg, char * dst_ip, char * dst_port){
 		return NULL;
 	}	
 	
+	// get host info
 	inet_pton(AF_INET, dst_ip, &temp);
 	if((h=gethostbyaddr(&temp,sizeof(temp),AF_INET)) == NULL) {
 		printf("UDP error: sending getting host information\n");
@@ -229,6 +238,7 @@ char * comUDP(char * msg, char * dst_ip, char * dst_port){
 
 	a=(struct in_addr*)h->h_addr_list[0];
 
+	// set remote host address and port for connection
 	memset((void*)&addr,(int)'\0',sizeof(addr));
 	addr.sin_family=AF_INET;
 	addr.sin_addr=*a;
@@ -236,7 +246,10 @@ char * comUDP(char * msg, char * dst_ip, char * dst_port){
 
 	printf("UDP Sending: %s\n", msg);
 
+	// all set up, send message
 	n=sendto(surDir_sock,msg,strlen(msg),0,(struct sockaddr*)&addr,sizeof(addr));
+
+	// check for errors on message send
 	if(n==-1) {
 		printf("UDP error: sending message to the server\n");
 		close(surDir_sock);
@@ -251,21 +264,24 @@ char * comUDP(char * msg, char * dst_ip, char * dst_port){
     	perror("Error");
 	}
 
+	// get incoming message response
 	addrlen = sizeof(addr);
 	n=recvfrom(surDir_sock,buffer,512,0,(struct sockaddr*)&addr,(socklen_t*)&addrlen);
 
+	// check for errors on message receive
 	if(n==-1) {
 		printf("UDP error: receiving message to the server\n");
 		close(surDir_sock);
 		return NULL;
 	}
 
-	answer=malloc(n);
+
+	answer=malloc(n); // alocate answer according to received bytes
 	sprintf(answer,"%.*s",n,buffer);
 
 	printf("Raw answer: %s\n",answer);
 
-	close(surDir_sock);
+	close(surDir_sock); // close udp socket
 
 	return answer;
 }
@@ -288,18 +304,22 @@ bool usrRegister(char * snpip, char * port, char * full_name, char * my_ip, char
 	char buffer[512];
 	char * answer;
 
+	// format user register message
 	sprintf(buffer,"REG %s;%s;%s",full_name,my_ip,my_port);
 
 	msg = malloc(strlen(buffer));
 	msg = buffer;
 
+	// send message through comUDP function
 	answer = comUDP(msg, snpip, port);
 
+	// check for error
 	if (answer == NULL){
 		printf("UDP error: empty message received\n");
 		return false;
 	}
 
+	// check for ack or error message
 	if(strcmp(answer,"OK") != 0) {
 		printf("Error: %s\n",answer);
 		free(answer);
@@ -327,18 +347,22 @@ bool usrExit(char * snpip, char * port, char * full_name){
 	char *answer;
 	char * msg;
 
+	// format user unregister message
 	sprintf(buffer,"UNR %s",full_name);
 
 	msg = malloc(strlen(buffer));
 	msg = buffer;
 
+	// send message through comUDP function
 	answer = comUDP(msg, snpip, port);
 
+	// check for error
 	if (answer == NULL){
 		printf("UDP error: empty message received\n");
 		return false;
 	}
 
+	// check for ack or error message
 	if(strcmp(answer,"OK") != 0) {
 		printf("Error: %s\n",answer);
 		return false;
@@ -366,18 +390,22 @@ char * queryUser(char * snpip, char * port, char * user){
 	char * msg;
 	char * location;
 
+	// format user query message
 	sprintf(buffer,"QRY %s",user);
 
 	msg = malloc(strlen(buffer));
 	msg = buffer;
 
+	// send message through comUDP function
 	answer = comUDP(msg, snpip, port);
 
+	// check for error
 	if (answer == NULL || strcmp(answer,"") == 0){
 		printf("UDP error: empty message received\n");
 		return "";
 	}
 
+	// check for query problems (ex: user not found)
 	if(strstr(answer,"NOK") != NULL) {
 		printf("Error: %s\n",answer);
 		return "";
@@ -385,6 +413,7 @@ char * queryUser(char * snpip, char * port, char * user){
 
 	memset(buffer, 0, sizeof(buffer));
 
+	// process information and store address data on buffer
 	sscanf(answer,"RPL %[^';'];%s", garb_0, buffer);
 
 	location = malloc(strlen(buffer));
@@ -445,11 +474,11 @@ int main(int argc, char* argv[]) {
 
 	int save_history = 0;
 
-	void (*old_handler)(int);//interrupt handler
+	void (*old_handler)(int); //interrupt handler
 
 	srand(time(NULL)); /* initializing pseudo-random number generator */
 
-	/*Check and retrieve given arguments	*/
+	/*Check and retrieve given arguments */
 	while((c=getopt(argc,argv,options)) != -1) {
 		switch(c)
 		{
@@ -484,35 +513,41 @@ int main(int argc, char* argv[]) {
 			return -1;
 		}
 	}
+
 	/* Check if required arguments are given   */
 	if(in_name_surname == NULL || in_snpip == NULL || in_snpport == NULL || in_scport == NULL || in_ip == NULL) {
 		show_usage();
 		return -1;
 	}
 
+	// show connection data for feedback
 	printf("Connection Data:\nLocalhost:%s:%s\nSurname Server:%s:%s\n",in_ip,in_scport,in_snpip,in_snpport);
 
+	// open main socket
 	if((fd=socket(AF_INET,SOCK_STREAM,0)) == -1){
+		printf("Cannot open main socket\n");
 		exit(1);
 	}
 
-	/* DEIXAMOS ISTO? É A MELHOR FORMA DE RESOLVER O PROBLEMA?? E O SIGPIPE???
-	 avoid the "Address already in use" error message immediately after closing socket  */
+	// avoid the "Address already in use" error message immediately after closing socket
 	if(setsockopt(fd,SOL_SOCKET,SO_REUSEADDR,&yes,sizeof(int)) == -1) { 
 		perror("setsockopt"); 
 		exit(1); 
 	}
 	
+	// format address for binding port 
 	memset((void*)&addr,(int)'\0',sizeof(addr));
 	addr.sin_family=AF_INET;
 	addr.sin_addr.s_addr=htonl(INADDR_ANY);
 	addr.sin_port=htons(atoi(in_scport));
 
+	// bind port to receive connections
 	if(bind(fd,(struct sockaddr*)&addr,sizeof(addr))==-1){
 		printf("ERROR: Cannot bind.\n");
 		exit(1);
 	}
 
+	// start listening
 	if(listen(fd,5)==-1){
 		printf("ERRO\n");
 		exit(1);
@@ -529,32 +564,38 @@ int main(int argc, char* argv[]) {
 
 	save_history = 1;
 
+	// From now on, the SIGPIPE signal will be ignored
 	if((old_handler=signal(SIGPIPE,SIG_IGN))==SIG_ERR)exit(1); //error
 
-	printf("Let's start..\n");
+	printf("All set! Let's start..\n");
 
 	while(1){
+		// initializing select function
 		FD_ZERO(&rfds);
 		FD_SET(fileno(stdin),&rfds);
 		FD_SET(fd,&rfds);
 		FD_SET(newfd,&rfds);
 		FD_SET(fd_out,&rfds);
 		
+		// set maximum file descriptor
 		maxfd = fileno(stdin);
 		maxfd = max(maxfd,fd);
 		maxfd = max(maxfd,newfd);
 		maxfd = max(maxfd,fd_out);
 
+		// clean user input variable
  		memset(usrIn, 0, sizeof(usrIn));
 
+ 		// select - blocks the code until some file descriptor has new data
 		counter=select(maxfd+1,&rfds,(fd_set*)NULL,(fd_set*)NULL,(struct timeval *)NULL);
 
+		// check for select errors
  		if(counter == -1){
  			printf("ERROR-: %s\n",strerror(errno));
  			exit(1);
  		}
 
-
+ 		// execute if stdin has new data - user types something and enter
  		if(FD_ISSET(fileno(stdin),&rfds)){
 			fgets(usrIn,100,stdin);
 
@@ -568,35 +609,48 @@ int main(int argc, char* argv[]) {
 				/* close(fd); exit(0); */
 
 			}else if(strcmp(usrIn,"leave\n") == 0 && stateMachine != init){
-				usrExit(in_snpip, in_snpport,in_name_surname);
+
+				// here, one can leave the network and, of course, the current conversation (if any)
+				if(!usrExit(in_snpip, in_snpport,in_name_surname)) {
+						printf(ANSI_COLOR_RED "Unregistration was not possible, leaving anyway...");
+						printf(ANSI_COLOR_WHITE "\n");
+					}
 				
+				// close any ongoing TCP connections 
 				shutdown(fd_out, SHUT_RDWR);
 				shutdown(newfd, SHUT_RDWR);
 
+				// reset status, allowing the user to rejoin the network
 				stateMachine = init;
 
 			}else if(strstr(usrIn,"find") != NULL && stateMachine != init){
 
-				memset(buffer, 0, sizeof(buffer));
+				// here, one can find other user on the network and get his connection data
 
-				sscanf(usrIn,"find %s", buffer);
+				memset(buffer, 0, sizeof(buffer)); // clean buffer
 
+				sscanf(usrIn,"find %s", buffer); // get user to search from stdin
+
+				// buffer processing to get the user name and memory allocation
 				name2connect = malloc(strlen(buffer));
-
-				strcpy(name2connect,buffer);
+				strcpy(name2connect,buffer); 
 
 				if (strlen(name2connect) <= 1){
+					// error check, no user written
 					printf("Argument missing. Who do you want to talk to?\n");
 				}else{
+					// got username, let's search for him - call queryUser function
 					location = queryUser(in_snpip, in_snpport, name2connect);
 
 					if (strcmp(location,"") == 0 || location == NULL){
+						// error check
 						printf("Ups.. User not found.\n");
 					}else{
+						// got user lcoation, give feedback
 						printf("User located at: %s\n", location);
 					}
 
-					memset(location, 0, sizeof(location));
+					memset(location, 0, sizeof(location)); // clean buffer
 				}
 
 			}else if(strstr(usrIn,"connect") != NULL && strcmp(usrIn,"disconnect\n") != 0 && stateMachine == registered){
@@ -702,55 +756,68 @@ int main(int argc, char* argv[]) {
 
 			}else if(strstr(usrIn,"message") != NULL && (stateMachine == onChat_sent || stateMachine == onChat_received) && stateMachine != init){
 
-				memset(buffer, 0, sizeof(buffer));
+				// here, some message is sent to someone
 
-				sscanf(usrIn,"message %[^\n]", buffer);
+				memset(buffer, 0, sizeof(buffer)); // clean buffer
 
+				sscanf(usrIn,"message %[^\n]", buffer); // get message to send
+
+				// send message to outgoing connection
 				if (stateMachine == onChat_sent){
 
 					nw = write(fd_out,buffer,strlen(buffer));
 
 				}else if(stateMachine == onChat_received){
+					// send message to incoming connection
 					nw = write(newfd,buffer,strlen(buffer));
 				}
 
+				// error check
 				if (nw <= 1){
-					printf("Could not send message.\n");
+					printf("ERROR: Could not send message.\n");
 				}
 
-				memset(buffer, 0, sizeof(buffer));
+				memset(buffer, 0, sizeof(buffer)); // clean buffer again
 
 			}else if(strcmp(usrIn,"disconnect\n") == 0 && (stateMachine == onChat_sent || stateMachine == onChat_received) && stateMachine != init){
 
-				if (stateMachine == onChat_sent){
+				// end conversation with someone
 
+				// shutdown outgoing connection
+				if (stateMachine == onChat_sent){
 					shutdown(fd_out, SHUT_RDWR);
 
 				}else if(stateMachine == onChat_received){
-					
+					// shutdown incomming connection
 					shutdown(newfd, SHUT_RDWR);
 
 				}
 
+				// update status to allow new connections
 				stateMachine = registered;
 
 			}else if(strcmp(usrIn,"exit\n") == 0){
 
+				// leave network (unregister from snp) if already registered
 				if(stateMachine != init){
+					// unregister and check for error
 					if(!usrExit(in_snpip, in_snpport,in_name_surname)) {
 						printf(ANSI_COLOR_RED "Unregistration was not possible, exiting anyway...");
 						printf(ANSI_COLOR_WHITE "\n");
 					}
 				}
 
+				// shutdown TCP connections
 				shutdown(fd_out, SHUT_RDWR);
 				shutdown(newfd, SHUT_RDWR);
 				shutdown(fd, SHUT_RDWR);
 
+				// close respective sockets
 				close(newfd);
 				close(fd_out);
 				close(fd);
 
+				// set file descriptors to NULL just to be sure
 				newfd = NULL;
 				fd = NULL;
 				fd_out = NULL;
@@ -758,6 +825,9 @@ int main(int argc, char* argv[]) {
 				printf("Exiting..\n");
 				break;
 			}else{
+
+				// print error messages for user feedback
+
 				if (strcmp(usrIn,"join\n") == 0){
 					printf("You already joined the network.\n");
 
@@ -795,10 +865,13 @@ int main(int argc, char* argv[]) {
 			}
 		}
 
+		// execute if main socket buffer has data
+		// here, new TCP connections to the main socket are processed
 		if(FD_ISSET(fd,&rfds)){
 
 			addrlen=sizeof(addr);
 
+			// if not connected with anyone -> accept connection
 			if (stateMachine == registered){
 
 				if((newfd=accept(fd,(struct sockaddr*)&addr,(socklen_t*)&addrlen))==-1){
@@ -811,16 +884,19 @@ int main(int argc, char* argv[]) {
 
 				stateMachine = onChat_authenticating_step_1;
 			}else{
+				// if connected with someone and other user tries to connect
+				// accept connection throught a temporary fd
 
 				if((refuse_fd=accept(fd,(struct sockaddr*)&addr,(socklen_t*)&addrlen))==-1){
 					printf("ERROR: Cannot refuse TCP.\n");
 					exit(1); 
 				}
 
+				// send busy line error message
 				sprintf(buffer,"Busy line. Connection Refused");
-
 				nw = write(refuse_fd,buffer,strlen(buffer));
 
+				// disconnect this new connection
 				shutdown(refuse_fd, SHUT_RDWR);
 
 				printf("Refused incoming connection because you are chatting with someone.\n");
@@ -828,8 +904,7 @@ int main(int argc, char* argv[]) {
 
 		}
 
-		/* Incoming connection */
-
+		// execute if Incoming connection socket buffer has data
 		if (newfd != NULL){
 
 			if(FD_ISSET(newfd,&rfds)){
@@ -917,8 +992,7 @@ int main(int argc, char* argv[]) {
 			}
 		}
 
-		/* Outgoing connection */
-
+		// execute if outgoing connection socket buffer has data
 		if (fd_out != NULL){
 			if(FD_ISSET(fd_out,&rfds)){
 
@@ -953,7 +1027,7 @@ int main(int argc, char* argv[]) {
 
 							}else{
 								encrypted = malloc(sizeof(unsigned char));
-								
+
 								encrypt(encrypted, randChar);
 
 								sprintf(buffer,"AUTH %c",*encrypted);
